d13cafcefdc2ad353d455255e0c18f5cbe09589f
Shoaib1M <shoaibmunavary@gmail.com>
2025-08-30 13:18:41 +0530

    fixingbugs2

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,16 +1,3 @@
-// import mongoose from "mongoose";
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(`${process.env.MONGODB_URI}/quickgpt`);
-//     console.log("database connected ");
-//   } catch (error) {
-//     console.log("DB Error:", error.message);
-//   }
-// };
-
-// export default connectDB;
-
 import mongoose from "mongoose";
 
 const connectDB = async () => {
